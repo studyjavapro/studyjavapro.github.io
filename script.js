37adc1cd99051b3cdf618d189596db6448ee59ad
studyjavapro <125212641+studyjavapro@users.noreply.github.com>
2023-02-17 17:36:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+    strings: [" was first released on 1995", " was released by Sun Microsystems"," is currently owned by Oracle"," is mainly used in Mobile Apps"," is Robust"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 2000,
+    loop: true
+})
+
+
